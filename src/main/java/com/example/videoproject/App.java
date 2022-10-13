c0b25bd0350bd6dbb7cd9ac3da533bee29234df8
Alexandre Massao <alexandremsubs@gmail.com>
2022-10-13 06:04:34 -0300

    added icon

--- src/main/java/com/example/videoproject/App.java
+++ src/main/java/com/example/videoproject/App.java
@@ -3,6 +3,7 @@ package com.example.videoproject;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +14,7 @@ public class App extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("mainGui.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Video Converter");
+        stage.getIcons().add(new Image("file:C:\\Users\\roadr\\Desktop\\simple-video-converter\\src\\video_tape.png"));
         //stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
